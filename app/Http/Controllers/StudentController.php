d0cf090b6de21d6c629e8e44c2ae755cfe3b28d9
ramzdf <ramazdf@gmail.com>
2021-10-17 19:32:10 +0700

    menambahkan fungsi edit data

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -43,4 +43,49 @@ class StudentController extends Controller
         return redirect()->route('students.index')
         ->with('success', 'Add data success!');
     }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $student = Student::find($id);
+        return view('students.edit',['student'=>$student]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        {
+            $student = Student::find($id);
+            $student->nim = $request->nim;
+            $student->name = $request->name;
+            $student->class = $request->class;
+            $student->department = $request->department;
+            $student->phone_number = $request->phone_number;
+            $student->save();
+            return redirect()->route('students.index');
+        }
+    }
+
 }

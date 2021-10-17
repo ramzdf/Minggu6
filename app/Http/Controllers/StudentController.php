26385e1e14e5d1f9d13d31c65608c94c714b03d1
ramzdf <ramazdf@gmail.com>
2021-10-17 19:00:29 +0700

    menampilkan data students

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Student::all();
+        return view('students.index',['student'=>$students]);
+    }
+
+
+}
